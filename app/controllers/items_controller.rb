21f515e7e841e94dca942451c9aca37c89571d1a
masu1031 <hibiki.masumoto@gmail.com>
2021-05-26 14:25:44 +0900

    直し１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :match_user, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,19 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless @item.user == current_user
-      redirect_to action: :index
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
     unless @item.update(item_params)
       render :edit
     else
@@ -44,4 +39,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :introduce, :category_id, :status_id, :burden_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])    
+  end
+
+  def match_user
+    unless @item.user == current_user
+      redirect_to action: :index
+    end
+  end
+
 end
