5fce9d69ec78a2762cf9c95439ff1981898bc35d
masu1031 <hibiki.masumoto@gmail.com>
2021-06-02 11:48:33 +0900

    直し１
    fix：
    ユーザー新規登録時のパスワードのvalidationを修正（７→６文字以上）

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,5 @@ class User < ApplicationRecord
   validates :lastname, :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :lastkana, :firstkana, format: { with: /\A[ァ-ヶー－]+\z/ }
 
-  validates :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
+  validates :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
 end
